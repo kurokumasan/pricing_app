7a819975a8851496d0dc4d980ac91a26ec0f9d84
kurokjumasan <daniel860126@gmail.com>
2022-08-14 14:23:34 +0000

    adding scrollview, not done yet

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
-#from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.core.text import LabelBase
 
 from pricing import price_estimation, price_detailed
 
@@ -10,6 +10,16 @@ class EstimationScreen(Screen):
     def switch_settlement(self):
         print('switch to settlement mode')
         App.get_running_app().root.current = 'Settlement'
+
+    def calculation(self, cost, host, host_no, guests, songs, group, guest_stage):
+        return price_estimation(\
+                int(cost), \
+                int(host), \
+                int(host_no), \
+                int(guests), \
+                int(songs), \
+                int(group), \
+                int(guest_stage))
 
 class SettlementScreen(Screen):
     def switch_estimation(self):
@@ -26,4 +36,11 @@ class PricingApp(App):
 
 
 if __name__ == '__main__':
+    LabelBase.register(name='NotoSans',\
+            fn_regular='/usr/share/fonts/truetype/notosans/NotoSans-Regular.ttf',\
+            fn_bold='/usr/share/fonts/truetype/notosans/NotoSans-Bold.ttf',\
+            fn_italic='/usr/share/fonts/truetype/notosans/NotoSans-Italic.ttf',\
+            fn_bolditalic='/usr/share/fonts/truetype/notosans/NotoSans-BoldItalic.ttf'\
+            )
+
     PricingApp().run()
--- pricing.py
+++ pricing.py
@@ -3,37 +3,41 @@ def get_int(x:float) -> int:
 
 def price_estimation(cost:int, host:int, host_no:int, guests:int, songs:int, group:int=5, guest_stage:int=2, place:str='表演場地'):
     
-    all_stages = group * songs
-    cost_per_song = cost / all_stages
-    guest_cost = guest_stage * cost_per_song
-    host_stages = all_stages - guest_stage * guests
-    host_cost = cost - guest_cost * guests
-
-    host_cost_per_song = host_cost / (host_stages + 1.5*host_no)
-    host_cost_without_stage = 1.5*host_cost_per_song
-    host_avg_stages = host_stages / (host-host_no)
-    host_avg_stage_cost = host_cost_per_song * host_avg_stages
-
-    print(f"估算方案：{place}");
-    print(f"【主辦】\n　上台：{host_cost_per_song}(元/首)")
-    print(f"平均接歌數量：{host_avg_stages}(首/人)")
-    print(f"平均付費金額：{host_avg_stage_cost}(元/人)")
-    print(f"沒上台：{host_cost_without_stage}(元/人)")
-    print(f"【協辦】\n{guest_cost}(元/人)")
-    print(f"\n///////////////若以10為基本單位取整數///////////////\n")
-    print(f"　【主辦】\n　上台：{get_int(host_cost_per_song)}(元/首)");
-    if (host_avg_stages==int(host_avg_stages)):
-        print(f"　平均接歌數量：{int(host_avg_stages)}(首/人)");
-        print(f"　平均付費金額：{int(host_avg_stages)*get_int(host_cost_per_song)}(元/人)");
-    else:
-        print(f"　平均接歌數量：{int(host_avg_stages)}到{int(host_avg_stages)+1}(首/人)");
-        print(f"　平均付費金額：{int(host_avg_stages)*get_int(host_cost_per_song)}到{(int(host_avg_stages)+1)*get_int(host_cost_per_song)}(元/人)");
-	
-    print(f"　沒上台：{get_int(host_cost_without_stage)}(元/人)");
-    print(f"　【協辦】\n　{get_int(guest_cost)}(元/人)");
-    income = get_int(host_cost_per_song)*host_stages+get_int(host_cost_without_stage)*host_no+get_int(guest_cost)*guests
-    print(f"　成本為{cost}元，總共收{income}元，最後會多出{income-cost}元納入社費");
-
+    try:
+        all_stages = group * songs
+        cost_per_song = cost / all_stages
+        guest_cost = guest_stage * cost_per_song
+        host_stages = all_stages - guest_stage * guests
+        host_cost = cost - guest_cost * guests
+    
+        host_cost_per_song = host_cost / (host_stages + 1.5*host_no)
+        host_cost_without_stage = 1.5*host_cost_per_song
+        host_avg_stages = host_stages / (host-host_no)
+        host_avg_stage_cost = host_cost_per_song * host_avg_stages
+    
+        result = f"估算方案：\n\
+                【主辦】\n　上台：{host_cost_per_song}(元/首)\n\
+                平均接歌數量：{host_avg_stages}(首/人)\n\
+                平均付費金額：{host_avg_stage_cost}(元/人)\n\
+                沒上台：{host_cost_without_stage}(元/人)\n\
+                【協辦】\n{guest_cost}(元/人)\n\
+                \n///////////////若以10為基本單位取整數///////////////\n\
+                【主辦】\n　上台：{get_int(host_cost_per_song)}(元/首)"
+        if (host_avg_stages==int(host_avg_stages)):
+            print(f"　平均接歌數量：{int(host_avg_stages)}(首/人)");
+            print(f"　平均付費金額：{int(host_avg_stages)*get_int(host_cost_per_song)}(元/人)");
+        else:
+            print(f"　平均接歌數量：{int(host_avg_stages)}到{int(host_avg_stages)+1}(首/人)");
+            print(f"　平均付費金額：{int(host_avg_stages)*get_int(host_cost_per_song)}到{(int(host_avg_stages)+1)*get_int(host_cost_per_song)}(元/人)");
+    	
+        print(f"　沒上台：{get_int(host_cost_without_stage)}(元/人)");
+        print(f"　【協辦】\n　{get_int(guest_cost)}(元/人)");
+        income = get_int(host_cost_per_song)*host_stages+get_int(host_cost_without_stage)*host_no+get_int(guest_cost)*guests
+        print(f"　成本為{cost}元，總共收{income}元，最後會多出{income-cost}元納入社費");
+        return result
+    except:
+        return 'Invalid input'
+    
 def price_detailed(cost:int, host:int, host_no:int, guests:int, all_stages:int, guest_stages:int, place:str='表演場地'):
     
     host_stages = all_stages - guest_stages
