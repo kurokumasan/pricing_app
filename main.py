4f58e18974fcacf254174bd71b0c8be30d075cd6
kurokjumasan <daniel860126@gmail.com>
2022-08-14 10:12:52 +0000

    handling multiple screens

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+#from kivymd.app import MDApp
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
 from pricing import price_estimation, price_detailed
 
 
-class PricingGrid(Widget):
-    pass
+class EstimationScreen(Screen):
+    def switch_settlement(self):
+        print('switch to settlement mode')
+        App.get_running_app().root.current = 'Settlement'
+
+class SettlementScreen(Screen):
+    def switch_estimation(self):
+        print('switch to estimation mode')
+        App.get_running_app().root.current = 'Estimation'
 
 class PricingApp(App):
     def build(self):
-        return PricingGrid() 
+        self.sm = ScreenManager(transition=NoTransition())
+        self.sm.add_widget(EstimationScreen(name='Estimation'))
+        self.sm.add_widget(SettlementScreen(name='Settlement'))
+
+        return self.sm
 
 
 if __name__ == '__main__':
